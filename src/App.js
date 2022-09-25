679773ef17d58c6024b5881a52a4ee5236c67cd4
JoseLuisCrego <crego.joseluis@gmail.com>
2022-09-25 17:37:43 +0200

    Cambios Jose

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import {
+  BrowserRouter, Routes,
+  Route
+} from "react-router-dom";
+//Components
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Tienda from './components/tienda/Tienda';
+import Formulario from './components/formulario/Formulario';
+import Carrito from './components/carrito/Carrito';
+import Inicio from './components/Home/Inicio';
+
+function App() {
+
+return (
+  <>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Inicio/>} /> 
+          <Route path="/formulario" element={<Formulario />} />
+      </Routes>
+    </BrowserRouter>
+  </>
+);
+}
+
+export default App;
+//(Revisar esto para el fallo de consola)https://es.reactjs.org/docs/lists-and-keys.html#gatsby-focus-wrapper
